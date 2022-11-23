b8f2a5ed307795075052aac65f7fe8c3b381146c
Phillip Zaya <Phillip.zaya@outlook.com>
2022-11-23 16:24:05 +1100

    fourth commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 
-export default function Blog() {
+const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+  `)
+  
   return (
     <Layout>
         <h1>Blog</h1>
-        <p>post will show here later on</p>
+        <ol>
+          {data.allMarkdownRemark.edges.map((edge) => {
+            return (
+              <li>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+              </li>
+            )
+          })}
+        </ol>
     </Layout>
   )
 }
+
+export default BlogPage
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2022-04-04"
+---
+
+I just launched a new bootcamp
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React
